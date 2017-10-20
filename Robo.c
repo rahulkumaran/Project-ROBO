467310c4d56ce4e011ec2f0d8728384b0c2ca164
Tarun Rao <tarunraok@outlook.com>
2017-10-20 23:59:48 +0530

    Update Robo.c
    Arms are fucked up due to DDA algorithm

--- Robo.c
+++ Robo.c
@@ -1,15 +1,13 @@
 #include <graphics.h>
-#include <stdio.h>
-
-float BX=320,BY=240,FX,FY;
+float FX,FY,BX,BY;
 void DDALine(float x1,float y1,float x2,float y2)
 {
+  float x,y,dx,dy,step;
   if(x1>x2 || y1>y2)
   {
-    float temp=x1;x1=x2;x2=temp;
-    temp=y1;y1=y2;y2=temp;
+    x1=x1+x2;x2=x1-x2;x1=x1-x2; //  x1 x2 swap
+    y1=y1+y2;y2=y1-y2;y1=y1-y2; //  y1 y2 swap
   }
-  float x,y,dx,dy,step;
   dx=abs(x2-x1);dy=abs(y2-y1);
   if(dx>=dy)
     step=dx;
@@ -30,46 +28,47 @@ void DDARect(float x1,float y1,float x2,float y2)
 }
 void main( )
 {
+  BX=320; BY=240; FX=BX; FY=BY-60;
   int gd=DETECT,gm;
   initgraph(&gd,&gm,"NULL");
-  //--Head
-  FX=BX;FY=BY-60;
-  //Cap
-  circle(FX,FY-40,5);
-  DDARect(FX-2.5,FY-25,FX+2.5,FY-35);
-  arc(FX,FY-20,180,360,5);
-  //Face
-  DDARect(FX-20,FY+20,FX+20,FY-20);//Face Outline
-  DDARect(FX-15,FY,FX-5,FY-10);//LEFT_EYE
-  DDARect(FX+5,FY,FX+15,FY-10);//RIGHT_EYE
-  //Mouth
-  DDARect(FX-10,FY+15,FX+10,FY+10);
-  //TEETH
-  DDARect(FX-2,FY+15,FX+2,FY+10);
-  DDARect(FX-6,FY+15,FX+6,FY+10);
-  //LEFT_EAR
-  DDARect(FX-25,FY,FX-20,FY-10);
-  DDARect(FX-30,FY-2.5,FX-25,FY-7.5);
-  //RIGHT_EAR
-  DDARect(FX+25,FY,FX+20,FY-10);
-  DDARect(FX+25,FY-2.5,FX+30,FY-7.5);
+  //--Head    
+    //Cap
+    circle(FX,FY-40,5);
+    DDARect(FX-2.5,FY-25,FX+2.5,FY-35);
+    arc(FX,FY-20,180,360,5);
+    //Face
+      DDARect(FX-20,FY+20,FX+20,FY-20);//Face Outline
+      DDARect(FX-15,FY,FX-5,FY-10);//LEFT_EYE
+      DDARect(FX+5,FY,FX+15,FY-10);//RIGHT_EYE
+      //Mouth
+        DDARect(FX-10,FY+15,FX+10,FY+10);
+        //TEETH
+        DDARect(FX-2,FY+15,FX+2,FY+10);
+        DDARect(FX-6,FY+15,FX+6,FY+10);
+      //LEFT_EAR
+      DDARect(FX-25,FY,FX-20,FY-10);
+      DDARect(FX-30,FY-2.5,FX-25,FY-7.5);
+      //RIGHT_EAR
+      DDARect(FX+25,FY,FX+20,FY-10);
+      DDARect(FX+25,FY-2.5,FX+30,FY-7.5);
+    DDARect(BX-5,BY-30,BX+5,BY-40);//Neck
   //--BODY
-  DDARect(BX-5,BY-30,BX+5,BY-40);//Neck
-  DDARect(BX-20,BY+15,BX+20,BY-20);//Body Square
-  DDARect(BX-30,BY+30,BX+30,BY-30);//Body
-  DDARect(BX-25,BY+70,BX-10,BY+30);//LLeg
-  arc(BX-17.5,BY+70,180,360,7.5);
-  DDARect(BX+25,BY+70,BX+10,BY+30);//RLeg
-  arc(BX+17.5,BY+70,180,360,7.5);
-  //Left_Hand
-  DDALine(BX-30,BY-15,BX-47,BY-5);
-  DDALine(BX-30,BY-5,BX-47,BY+5);
-  circle(BX-47,BY,5);
-  //Right_Hand
-  DDALine(BX+47,BY-5,BX+30,BY-15);
-  DDALine(BX+47,BY+5,BX+30,BY-5);
-  circle(BX+47,BY,5);
-
-  delay(5000);
-  getch();
+    DDARect(BX-30,BY+30,BX+30,BY-30); // Body Outline
+    DDARect(BX-20,BY+15,BX+20,BY-20); // Inner Square
+    DDARect(BX-25,BY+70,BX-10,BY+30); //  Left Leg
+    arc(BX-17.5,BY+70,180,360,7.5);  // Left Foot
+    DDARect(BX+25,BY+70,BX+10,BY+30); //  Right Leg
+    arc(BX+17.5,BY+70,180,360,7.5); //  Right Foot
+    //Left_Hand
+    //DDA FUCK :: ADD CODE HERE
+    circle(BX-47,BY,5);
+    DDALine(BX-47,BY-25,BX-64,BY-15);
+    DDALine(BX-47,BY-15,BX-64,BY-5);
+    //Right_Hand
+    DDALine(BX+47,BY-5,BX+30,BY-15);
+    DDALine(BX+47,BY+5,BX+30,BY-5);
+    circle(BX+47,BY,5);
+    //DDA Fuck :: ADD CODE HERE
+  //END
+  delay(5000);  getch();
 }
